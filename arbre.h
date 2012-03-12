75126d4be1637e3d88fd3215830a413c781cc795
guparan <guparan@60d5f5f2-5f68-30ba-df4e-131dfe72b36e>
2012-03-12 18:58:30 +0000

    Implementation des fonctions de base du type arbre

--- arbre.h
+++ arbre.h
@@ -2,29 +2,29 @@
 #define __ARBRE_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "bool.h"
 
-// 0 : fils gauche, 1 : fils droit
+/* 0 : fils gauche, 1 : fils droit */
 typedef struct noeud {
 	unsigned char c;
 	int freq;
-	struct noeud *fg, *fd; // fils gauche, fils droit
+	struct noeud *fg, *fd; /* fils gauche, fils droit */
 } *Arbre;
 
 Arbre creerFeuille(unsigned char c, int freq);
-Arbre enrac(Arbre a_gauche, Arbre a_droit); // freq = freq de a_gauche + freq
-a_droit
+Arbre enrac(Arbre a_gauche, Arbre a_droit); /* freq = freq de a_gauche + freq de a_droit */
 
-// selecteurs et tests élémentaires
+/* selecteurs et tests élémentaires */
 Arbre fg(Arbre);
 Arbre fd(Arbre);
 int frequenceRacine(Arbre);
 bool estVide(Arbre);
 bool estFeuille(Arbre);
 unsigned char carRacine(Arbre);
-// affichage infixe d'un arbre (fg - racine - fd)
-void afficheArbre(Arbre);
-// construction de l'arbre de huffman à partir d'un tableau de fréquence
+/* affichage infixe d'un arbre (fg - racine - fd) */
+void afficheArbre(Arbre, void(*f)(char, int));
+/* construction de l'arbre de huffman à partir d'un tableau de fréquence */
 Arbre construitArbre(int freq[256]);
 
 #endif
