dd102bcfec9f81d3f833484b25fd16c9bb04433b
LucasSaurel <LucasSaurel@60d5f5f2-5f68-30ba-df4e-131dfe72b36e>
2012-03-13 20:44:40 +0000

    Modification de la structure des arbres: utilisation de char* au lieu de char

--- trunk/arbre.h
+++ trunk/arbre.h
@@ -7,12 +7,12 @@
 
 /* 0 : fils gauche, 1 : fils droit */
 typedef struct noeud {
-	unsigned char c;
+	unsigned char* c;
 	int freq;
 	struct noeud *fg, *fd; /* fils gauche, fils droit */
 } *Arbre;
 
-Arbre creerFeuille(unsigned char c, int freq);
+Arbre creerFeuille(unsigned char* c, int freq);
 Arbre enrac(Arbre a_gauche, Arbre a_droit); /* freq = freq de a_gauche + freq de a_droit */
 
 /* selecteurs et tests élémentaires */
@@ -21,9 +21,9 @@ Arbre fd(Arbre);
 int frequenceRacine(Arbre);
 bool estVide(Arbre);
 bool estFeuille(Arbre);
-unsigned char carRacine(Arbre);
+unsigned char* carRacine(Arbre);
 /* affichage infixe d'un arbre (fg - racine - fd) */
-void afficheArbre(Arbre, void(*f)(char, int));
+void afficheArbre(Arbre, void(*f)(char*, int));
 void parseArbre(Arbre, FILE*);
 void afficheArbreDot(Arbre, char*);
 /* construction de l'arbre de huffman à partir d'un tableau de fréquence */
