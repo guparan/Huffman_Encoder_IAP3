4284d8478e2f6acbdbbf2a626598f9d9412d6f8a
LucasSaurel <LucasSaurel@60d5f5f2-5f68-30ba-df4e-131dfe72b36e>
2012-03-14 18:53:23 +0000

    Ajout de commentaires (Message subliminal: Xcode c'est trop bien)

--- listeTrieeArbres.h
+++ listeTrieeArbres.h
@@ -1,17 +1,48 @@
 #ifndef LISTETRIEEARBRES_H
 #define LISTETRIEEARBRES_H
 
+#include "arbre.h"
+
 
 typedef struct element {
 	Arbre arbre;
 	struct element* suivant;
 } *ListeTrieeArbres;
 
-/****************************************************/
 
+/**
+ * \fn ListeTrieeArbres creerListe(void);
+ * \brief Renvoie NULL
+ * 
+ **/
 ListeTrieeArbres creerListe(void);
+
+
+/**
+ * \fn ListeTrieeArbres insererArbreListe(ListeTrieeArbres, Arbre);
+ * \brief Renvoie une liste construite à partir de la liste et de l'arbre en paramètres
+ * 
+ * \param l liste d'arbres triée
+ * \param a arbre à insérer
+ **/
 ListeTrieeArbres insererArbreListe(ListeTrieeArbres, Arbre);
+
+
+/**
+ * \fn Arbre teteListe(ListeTrieeArbres);
+ * \brief Renvoie l'arbre en tête de la liste en paramètre (fréquence la plus faible de la liste)
+ * 
+ * \param l liste d'arbres triée
+ **/
 Arbre teteListe(ListeTrieeArbres);
+
+
+/**
+ * \fn ListeTrieeArbres supprimerTeteListe(ListeTrieeArbres);
+ * \brief Supprime le premier arbre de la liste en paramètre et renvoie la nouvelle liste
+ * 
+ * \param l liste d'arbres triée
+ **/
 ListeTrieeArbres supprimerTeteListe(ListeTrieeArbres);
 
 
