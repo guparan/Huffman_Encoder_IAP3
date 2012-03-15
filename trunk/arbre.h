af764f13fa64753a0645a41f073c11242cd77f3e
guparan <guparan@60d5f5f2-5f68-30ba-df4e-131dfe72b36e>
2012-03-15 15:08:03 +0000

    Reprise de insererArbre, renommage de toutes les fonctions pour acces plus simple

--- trunk/arbre.h
+++ trunk/arbre.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "bool.h"
 
 
@@ -14,23 +15,23 @@ typedef struct noeud {
 
 
 /**
- * \fn Arbre creerFeuille(unsigned char c, int freq);
+ * \fn Arbre arbre_creerFeuille(unsigned char c, int freq);
  * \brief Cree un arbre contenant une unique feuille
  * 
  * \param c caractère à assigner à la feuille
  * \param freq fréquence associée au caractère c
  **/
-Arbre creerFeuille(unsigned char c, int freq);
+Arbre arbre_creerFeuille(unsigned char c, int freq);
 
 
 /**
- * \fn Arbre enrac(Arbre a_gauche, Arbre a_droit);
+ * \fn Arbre arbre_enrac(Arbre a_gauche, Arbre a_droit);
  * \brief Renvoie un arbre ayant pour fils gauche et droit les deux arbres en paramètre et pour fréquence la somme des fréquences des arbres en paramètre.
  * 
  * \param a_gauche arbre à placer en tant que fils gauche
  * \param a_droit arbre à placer en tant que fils droit
  **/
-Arbre enrac(Arbre a_gauche, Arbre a_droit); /* freq = freq de a_gauche + freq de a_droit */
+Arbre arbre_enrac(Arbre a_gauche, Arbre a_droit); /* freq = freq de a_gauche + freq de a_droit */
 
 
 
@@ -38,57 +39,57 @@ Arbre enrac(Arbre a_gauche, Arbre a_droit); /* freq = freq de a_gauche + freq de
 /* selecteurs et tests élémentaires */
 
 /**
- * \fn Arbre fg(Arbre);
+ * \fn Arbre arbre_fg(Arbre);
  * \brief Retourne le fils gauche de l'arbre en paramère
  * 
  * \param arbre 
  **/
-Arbre fg(Arbre);
+Arbre arbre_fg(Arbre);
 
 
 /**
- * \fn Arbre fd(Arbre);
+ * \fn Arbre arbre_fd(Arbre);
  * \brief Retourne le fils droit de l'arbre en paramère
  * 
  * \param arbre 
  **/
-Arbre fd(Arbre);
+Arbre arbre_fd(Arbre);
 
 
 /**
- * \fn int frequenceRacine(Arbre);
+ * \fn int arbre_frequenceRacine(Arbre);
  * \brief Retourne la fréquence de la racine de l'arbre en paramètre
  * 
  * \param arbre
  **/
-int frequenceRacine(Arbre);
+int arbre_frequenceRacine(Arbre);
 
 
 /**
- * \fn bool estVide(Arbre);
+ * \fn bool arbre_estVide(Arbre);
  * \brief Teste si un arbre est vide: renvoie true (1) si vide et false (0) sinon
  * 
  * \param arbre
  **/
-bool estVide(Arbre);
+bool arbre_estVide(Arbre);
 
 
 /**
- * \fn bool estFeuille(Arbre);
+ * \fn bool arbre_estFeuille(Arbre);
  * \brief Renvoie true (1) si l'arbre en paramètre est une feuille et false (0) sinon
  * 
  * \param arbre
  **/
-bool estFeuille(Arbre);
+bool arbre_estFeuille(Arbre);
 
 
 /**
- * \fn unsigned char carRacine(Arbre);
+ * \fn unsigned char arbre_carRacine(Arbre);
  * \brief Renvoie le caractère de la racine de l'arbre en paramètre. Si ce n'est pas une feuille, la fonction renvoie le caractère nul (\0).
  * 
  * \param arbre
  **/
-unsigned char carRacine(Arbre);
+unsigned char arbre_carRacine(Arbre);
 
 
 
@@ -96,33 +97,33 @@ unsigned char carRacine(Arbre);
 /* affichage infixe d'un arbre (fg - racine - fd) */
 
 /**
- * \fn void afficheArbre(Arbre, void(*f)(unsigned char, int));
+ * \fn void arbre_afficheArbre(Arbre, void(*f)(unsigned char, int));
  * \brief affiche l'arbre en paramètre en utilisant la fonction f donnée en deuxième paramètre
  * 
  * \param arbre à afficher
  * \param f fonction d'affichage
  **/
-void afficheArbre(Arbre, void(*f)(unsigned char, int));
+void arbre_afficheArbre(Arbre, void(*f)(unsigned char, int));
 
 
 /**
- * \fn void parseArbre(Arbre, FILE*);
+ * \fn void arbre_parseArbre(Arbre, FILE*);
  * \brief Ecrit dans le fichier en paramètre le code décrivant les arêtes pour l'affichage DOT de l'arbre
  * 
  * \param arbre à afficher
  * \param fichier pointeur sur le fichier dans lequel il faut écrire
  **/
-void parseArbre(Arbre, FILE*);
+void arbre_parseArbre(Arbre, FILE*);
 
 
 /**
- * \fn void afficheArbreDot(Arbre, char*);
+ * \fn void arbre_afficheArbreDot(Arbre, char*);
  * \brief Ecrit dans le fichier en paramètre le code permettant de générer l'affichage de l'arbre en DOT
  * 
  * \param arbre à afficher
  * \param fichier pointeur sur le fichier dans lequel il faut écrire
  **/
-void afficheArbreDot(Arbre, char*);
+void arbre_afficheArbreDot(Arbre, char*);
 
 
 
@@ -130,12 +131,12 @@ void afficheArbreDot(Arbre, char*);
 /* construction de l'arbre de huffman à partir d'un tableau de fréquence */
 
 /**
- * \fn Arbre construitArbre(int freq[256]);
+ * \fn Arbre arbre_construitArbre(int freq[256]);
  * \brief Renvoie l'arbre de Huffman construit à partir du tableau de fréquences en paramètre.
  * 
  * \param freq tableau des fréquences d'apparition des caractères ASCII
  **/
-Arbre construitArbre(int freq[256]);
+Arbre arbre_construitArbre(int freq[256]);
 
 
 /*
