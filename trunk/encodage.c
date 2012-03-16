5d98896beeb6c501778fc83486abf057a3b98c25
guparan <guparan@60d5f5f2-5f68-30ba-df4e-131dfe72b36e>
2012-03-16 23:01:31 +0000

    Avancement de preEncode : a terminer

--- trunk/encodage.c
+++ trunk/encodage.c
@@ -12,14 +12,32 @@ void analyseFichier(FILE *fp, int freq[256])
 }
 
 
-char** encodage_preEncode(Arbre a)
+char** encodage_preEncode(Arbre a, char* construct)
 {
 	/* Parcours de l'arbre
 	 * construction d'une chaine de 1 et 0
 	 * a chaque car rencontré, copie de la chaine à l'indice ASCII 
 	 * retourne le tableau des chaines */
-	 
-	 	 
+	char** binaire;
+	
+	binaire = (char**)malloc(arbre_nbFeuilles(a)*sizeof(char*));
+	
+	if(arbre_estFeuille(a))
+	{
+		binaire[a->c]=construct;
+	}
+	
+	if(a->fg)
+	{
+		/* rajouter un 0 a construct et descendre dans le fils gauche */
+		return encodage_preEncode(a->fg, construct);
+	}
+	if(a->fd)
+	{
+		/* rajouter un 1 a construct et descendre dans le fils droit */
+		return encodage_preEncode(a->fd, construct);
+	}
+	
 }
 
 
@@ -72,24 +90,24 @@ char decode(Arbre a, char* code)
     {
         if (code[i] == '0')
         {
-            courant = arbre_fg(courant);    //on part dans le fils gauche
+            courant = arbre_fg(courant);    /*on part dans le fils gauche*/
         }
         else if (code[i] == '1')
         {
-            courant = arbre_fd(courant);    //on part dans le fils droit
+            courant = arbre_fd(courant);    /*on part dans le fils droit*/
         }
         else
         {
-            return res;     //on sort de la fonction, le code en paramètre est erroné
+            return res;     /*on sort de la fonction, le code en paramètre est erroné*/
         }
-        i++;    //passage au caractère suivant dans le code
+        i++;    /*passage au caractère suivant dans le code*/
     }
     
-    if (arbre_estFeuille(courant))   //on vérifie que courant est une feuille avant d'affecter la valeur de res
+    if (arbre_estFeuille(courant))   /*on vérifie que courant est une feuille avant d'affecter la valeur de res*/
     {
         res = arbre_carRacine(courant);
     }
-    //si courant n'est pas une feuille, res est inchangé est vaut '\0'
+    /*si courant n'est pas une feuille, res est inchangé est vaut '\0'*/
     
     
     return res;
