5655170ce23560d4e952647997ba2df65962f273
LucasSaurel <LucasSaurel@60d5f5f2-5f68-30ba-df4e-131dfe72b36e>
2012-03-19 20:17:58 +0000

    Fonction décompresse débuguée; pb -> fichier décompressé vide

--- trunk/decompression.c
+++ trunk/decompression.c
@@ -26,12 +26,12 @@ FILE* decompression_decompresse(char* nomFichier)
    /* Création de la table des fréquences à utiliser (par lecture de fichierCodage) */
     indice = malloc(10*sizeof(char));
     frequence = malloc(1024*sizeof(char));
-    do
+    while (!feof(fichierCodage))
     {
-        fgets(indice, 4, fichierComp);   /* on lit l'indice (si EOF, indice = NULL) */
-        fgets(frequence, 1024, fichierComp);    /* on lit la fréquence correspondante */
+        fgets(indice, 6, fichierCodage);   /* on lit l'indice */
+        fgets(frequence, 1024, fichierCodage);    /* on lit la fréquence correspondante */
         freq[atoi(indice)] = atoi(frequence); /* atoi : conversion string to int */        
-    } while (indice != NULL);
+    }
     
     free(indice);
     free(frequence);
@@ -40,11 +40,12 @@ FILE* decompression_decompresse(char* nomFichier)
     
     /* Création de l'arbre de Huffman à partir du tableau des fréquences */
     huffman = liste_construitArbre(liste_construitListeArbres(freq));
-    
+
     
     /* Ouverture du fichier compressé et test d'ouverture */
     nomFichierComp=(char*)malloc(strlen(nomFichier)+strlen(extensionFichComp)+1);    /* malloc de nomFichierComp pour ajouter l'extension */
     strcpy(nomFichierComp, nomFichier);
+    free(nomFichierComp);
     fichierComp = fopen(strcat(nomFichierComp, extensionFichComp), "rb");   /* Ouverture du fichier en mode */
     if (fichierComp == NULL)        /* Test d'ouverture */
     {
@@ -52,23 +53,23 @@ FILE* decompression_decompresse(char* nomFichier)
         exit(errno);
     }
     
-    
     /* Creation du fichier décompressé et test d'ouverture */
     nomFichierDecomp=(char*)malloc(strlen(nomFichier)+strlen(extensionFichDecomp)+1);    /* malloc de nomFichierDecomp pour ajouter l'extension */
     strcpy(nomFichierDecomp, nomFichier);
-    fichierDecomp = fopen(strcat(nomFichier, extensionFichDecomp), "w+");
+    fichierDecomp = fopen(strcat(nomFichierDecomp, extensionFichDecomp), "w+");
+    free(nomFichierDecomp);
     if (fichierDecomp == NULL)        /* Test d'ouverture */
     {
         perror("fopen");
         exit(errno);
     }
-    free(nomFichier);
     
     /* On lit fichierComp (en binaire) en parcourant l'arbre de huffman et on écrit dans fichierDecomp */
     courant = huffman;  /* On initialise courant en lui affectant huffman */
-    caractereActuel = fgetc(fichierComp);   /* lecture du premier caractère */
-    while (caractereActuel != EOF)
+    
+    while (!feof(fichierComp))
     {
+        caractereActuel = fgetc(fichierComp);   /* lecture d'un caractère */
         if (caractereActuel == '0')
         {
             courant = arbre_fg(courant);
@@ -83,11 +84,10 @@ FILE* decompression_decompresse(char* nomFichier)
             fputc(arbre_carRacine(courant), fichierDecomp);
             courant = huffman;
         }
-        caractereActuel = fgetc(fichierComp);
     }
     
     fclose(fichierComp);    /* Fin des opérations sur fichierComp */
     free(huffman);  /* On libère la mémoire utilisée par l'arbre de huffman */
-    
+
     return fichierDecomp;
 }
