b0a12e392c31b7a166fc060c811f842bc944df68
guparan <guparan@60d5f5f2-5f68-30ba-df4e-131dfe72b36e>
2012-03-19 10:03:40 +0000

    Ajout de tests.h

---
+++ tests.h
@@ -0,0 +1,29 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#include <stdio.h>
+#include "arbre.h"
+#include "listeArbres.h"
+#include "encodage.h"
+#include "op_bits.h"
+#include "compression.h"
+#include "decompression.h"
+
+void my_display(unsigned char c, int f);
+
+Arbre test_enracArbre(void);
+
+void test_afficheArbre(void);
+
+void test_insererTriArbre(void);
+
+void test_encodage_analyseFichierConstruitArbre(void);
+
+void test_encode(void);
+
+void test_decode(void);
+
+void test_compresse(void);
+
+
+#endif
